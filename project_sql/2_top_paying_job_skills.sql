948842565c96421bc3de559c0ffd0ff22e570025
dataral <aral.oktar1@gmail.com>
2026-08-14 14:40:45 +0300

    top_paying_job_skills

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,10 +1,9 @@
+WITH top_paying_remote_jobs AS
+(
 select  
 job_id,
 job_title,
-job_location,
-job_schedule_type,
 salary_year_avg,
-job_posted_date,
 name AS company_name
 from job_postings_fact
 left JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
@@ -12,4 +11,11 @@ WHERE job_title = 'Data Analyst' AND
 job_location = 'Anywhere' AND
 salary_year_avg IS NOT NULL
 order by salary_year_avg desc
-limit 10;
+limit 10
+)
+
+select top_paying_remote_jobs.*,
+skills
+from top_paying_remote_jobs
+inner JOIN skills_job_dim ON top_paying_remote_jobs.job_id = skills_job_dim.job_id
+inner JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
