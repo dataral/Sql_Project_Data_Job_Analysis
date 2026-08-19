5c9cccdbad6410a0f3352541810aacbc25946d91
dataral <aral.oktar1@gmail.com>
2026-08-19 12:18:23 +0300

    top paying skills and optimal skills queries

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,15 +1,15 @@
-
 SELECT 
 skills,
-count(skills_job_dim.job_id) as demand_count
+round(AVG(salary_year_avg), 0) as avg_salary
 from job_postings_fact
 
 inner join skills_job_dim
 on job_postings_fact.job_id = skills_job_dim.job_id
 inner join skills_dim
 on skills_job_dim.skill_id = skills_dim.skill_id
-where job_title_short = 'Data Analyst' AND
-job_work_from_home = TRUE
+where job_title_short = 'Data Analyst'
+ANd salary_year_avg IS NOT NULL
+AND job_work_from_home = TRUE
 group by skills
-order by demand_count desc
-limit 5;   
+order by avg_salary desc
+limit 25;   
